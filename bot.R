9fd8414884294e3cb230c05d60f59ca4407b1f14
Yᴀsʜ <unknownsoumo@gmail.com>
2024-10-15 19:11:55 +0530

    good !

--- bot.R
+++ bot.R
@@ -4,6 +4,7 @@ bot_token <- "7855855349:AAHxzMPx1Ggk0kba97ryJ06l3NG8M98E0fc"
 bot <- Bot(token = bot_token)
 
 source("modules/update.R")
+source("modules/ping.R")
 
 start_handler <- function(bot, update) {
   user_name <- update$message$from$first_name
@@ -14,6 +15,7 @@ start_handler <- function(bot, update) {
 updater <- Updater(token = bot_token)
 updater <- updater + CommandHandler("start", start_handler)
 updater <- updater + CommandHandler("update", update_handler)
+updater <- updater + CommandHandler("ping", ping_handler)
 
 updater$start_polling()
 invisible(readline(prompt = "Press [Enter] To Stop The Bot."))
