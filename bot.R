7f07c836977b8c9c1896134dbc970267dbeba7bc
Yᴀsʜ <unknownsoumo@gmail.com>
2024-10-15 18:32:22 +0530

    good !

--- bot.R
+++ bot.R
@@ -3,12 +3,11 @@ library(telegram.bot)
 bot_token <- "7855855349:AAHxzMPx1Ggk0kba97ryJ06l3NG8M98E0fc"
 bot <- Bot(token = bot_token)
 
-start_handler <- function(bot, update) {
-  bot$sendMessage(chat_id = update$message$chat_id, text = "Hello! I am your bot.")
-}
+source("modules/update.R")
 
 updater <- Updater(token = bot_token)
 updater <- updater + CommandHandler("start", start_handler)
+updater <- updater + CommandHandler("update", update_handler)
 
 updater$start_polling()
-invisible(readline(prompt = "Press [Enter] to stop the bot."))
+invisible(readline(prompt = "Press [Enter] To Stop The Bot."))
