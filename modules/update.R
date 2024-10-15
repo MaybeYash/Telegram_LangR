e861603ab209e0481c706c6efb702d9684f8a554
Yᴀsʜ <unknownsoumo@gmail.com>
2024-10-15 19:18:28 +0530

    good !

--- modules/update.R
+++ modules/update.R
@@ -1,18 +1,26 @@
 update_handler <- function(bot, update) {
   chat_id <- update$message$chat_id
+  user_id <- update$message$from$id
+  message_id <- update$message$message_id
+  
   tryCatch({
+    bot$sendMessage(chat_id = chat_id, text = "Updating the bot from GitHub...", reply_to_message_id = message_id)
     out <- system("git pull", intern = TRUE)
     output_message <- paste(out, collapse = "\n")
     
-    if (grepl("Already up to date.", output_message)) {
-      bot$sendMessage(chat_id = chat_id, text = "It's already up-to-date !")
-    } else {
-      bot$sendMessage(chat_id = chat_id, text = paste0("```", output_message, "```"), parse_mode = "Markdown")
-    }
+    bot$sendMessage(chat_id = chat_id, text = paste0("```", output_message, "```"), parse_mode = "Markdown")
+    bot$sendMessage(chat_id = chat_id, text = "Update complete! Restarting bot in 1 minute.", reply_to_message_id = message_id)
     
-    bot$sendMessage(chat_id = chat_id, text = "Updated with the default branch, restarting now.")
+    Sys.sleep(60)
+    
+    bot$sendMessage(chat_id = chat_id, text = "Restarting bot now...")
+    
+    r_bg(function() {
+      system("Rscript bot.R")
+    })
+    quit("no")
     
   }, error = function(e) {
-    bot$sendMessage(chat_id = chat_id, text = as.character(e))
+    bot$sendMessage(chat_id = chat_id, text = as.character(e), reply_to_message_id = message_id)
   })
 }
